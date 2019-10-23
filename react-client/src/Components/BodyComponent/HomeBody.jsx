68a5dcb5314f1418b0dab015de4709e2469879c5
b-esc <bescobar@iastate.edu>
2019-10-22 20:39:12 -0500

    full-o-deadlinks

--- react-client/src/Components/BodyComponent/HomeBody.jsx
+++ react-client/src/Components/BodyComponent/HomeBody.jsx
@@ -19,6 +19,144 @@ import {
   Visibility
 } from "semantic-ui-react";
 
+const particlesjsConfig = {
+  "particles": {
+    "number": {
+      "value": 104,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#e5bceb"
+    },
+    "shape": {
+      "type": "star",
+      "stroke": {
+        "width": 1,
+        "color": "#cf9e9e"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "opacity_min": 0,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 4,
+        "size_min": 0.3,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 1.5,
+      "direction": "bottom",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 600
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 250,
+        "size": 0,
+        "duration": 2,
+        "opacity": 0,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 400,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+};
+
+const AboutText = props => (
+    <div className="bodyText">
+    <ul>
+    <li>
+    Hey, I'm Ben.
+    </li>
+    <li>
+    Thank you for visiting my personal site.
+    </li>
+    <li>
+    I'm currently a senior in Computer Science and Bioinformatics at Iowa State University.
+    </li>
+    <li>
+    I founded both the Linux Club and Programming Interview Club during my time here.
+    </li>
+    <li>
+    Come June 2020, I'll be working as a Software Engineer at Facebook.
+    </li>
+    <li>
+    Linux, Software Engineering, and Biology comprise the bulk of my interests,
+    often complementing one another on account of similar tooling.
+    </li>
+    </ul>
+    </div>
+  );
+
+
 class HomeBody extends Component{
   render (){
       return(
@@ -31,19 +169,17 @@ class HomeBody extends Component{
         >
           <Grid className="border_1px particlesFg">
           <div id="particlesBg">
-              <Particles />
+              <Particles params={particlesjsConfig} />
           </div>
-            <Grid.Column width={6}>
+            <Grid.Column width={7}>
             About
             <br></br>
-            <div className="bodyText">
-            aboutme et al
-            </div>
+            <AboutText/>
             </Grid.Column>
-            <Grid.Column width={3}>
-            Filler
+            <Grid.Column width={2}>
+
             </Grid.Column>
-            <Grid.Column width={5}>
+            <Grid.Column width={6}>
             Contact
             <br></br>
             <ul className="contactText">
